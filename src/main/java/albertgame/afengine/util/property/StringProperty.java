c2f158c3bea1ede8a2fe66d16338581555505d4c
albert-flex <albert-flex@albertpc>
2020-04-02 00:14:02 +0800

    imple util.property

---
+++ src/main/java/albertgame/afengine/util/property/StringProperty.java
@@ -0,0 +1,18 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package albertgame.afengine.util.property;
+
+/**
+ *
+ * @author albert-flex
+ */
+public class StringProperty extends AbValueProperty<String>{
+
+    public StringProperty(String value) {
+        super(value);
+    }
+    
+}
