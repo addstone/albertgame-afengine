a916faeaeaf1a175dfa91052ebe06ff40d15278d
Albert Flex <2662842460@qq.com>
2020-04-02 22:38:36 +0800

    test xmlutil

--- src/main/java/albertgame/afengine/util/XmlUtil.java
+++ src/main/java/albertgame/afengine/util/XmlUtil.java
@@ -8,8 +8,8 @@ package albertgame.afengine.util;
 import java.io.File;
 import java.io.FileOutputStream;
 import java.io.IOException;
-import java.util.logging.Level;
-import java.util.logging.Logger;
+import java.net.URISyntaxException;
+import java.net.URL;
 import org.dom4j.Document;
 import org.dom4j.DocumentException;
 import org.dom4j.DocumentHelper;
@@ -23,73 +23,86 @@ import org.dom4j.io.XMLWriter;
  */
 public class XmlUtil {
 
-    private static final Logger log = Logger.getLogger(XmlUtil.class.getName());
-        
-    public static Document getXMLWritableDocument(String xmlpath){
-        if(xmlpath==null){
-            Document doc=DocumentHelper.createDocument();
-            doc.addElement("root");
-            return doc;
-        }
-
-        File file = new File(xmlpath);
-        if(!file.exists()){
-            Document doc=DocumentHelper.createDocument();
-            doc.addElement("root");
-            return doc;            
-        }
-        SAXReader reader =new SAXReader();
-        Document doc;
-        try{
-            doc = reader.read(file);            
-            log.log(Level.INFO,"root : "+doc.getRootElement().getName());
-        }catch(DocumentException e){
-            e.printStackTrace();
-            return null;
-        }
-        return doc;        
-    }
-    public static Document readXMLFileDocument(String xmlpath){
-        if(xmlpath==null){
-            log.log(Level.INFO,"read xml can not null!");
-            return null;
-        }
-
-        File file = new File(xmlpath);
-        if(!file.exists()){
-            log.log(Level.INFO,"the xml file - "+xmlpath+" is not exist.");
-            return null;
-        }
-        SAXReader reader =new SAXReader();
-        Document doc;
-        try{
-            doc = reader.read(file);            
-            log.log(Level.INFO,"root : "+doc.getRootElement().getName());
-        }catch(DocumentException e){
-            e.printStackTrace();
-            return null;
-        }
-        return doc;
-    }
-    public static void writeXMLFile(String xmlpath,Document doc){
-        FileOutputStream out =null;
+    private static Document readXML(URL url, String xmlpath, boolean isUrl, boolean create) {
+        File file = null;
         try {
-            out = new FileOutputStream(xmlpath);
-            OutputFormat format=OutputFormat.createPrettyPrint();   //漂亮格式：有空格换行
+            if (isUrl) {
+                file = new File(url.toURI());
+            } else {
+                if (xmlpath == null) {
+                    System.out.println("read xml can not null!");
+                    return null;
+                }
+                file = new File(xmlpath);
+            }
+            if (!file.exists()) {
+                Document doc;
+                if (create) {
+                    doc = DocumentHelper.createDocument();
+                    doc.addElement("root");
+                } else {
+                    doc = null;
+                }
+                return doc;
+            }
+            SAXReader reader = new SAXReader();
+            Document doc;
+            try {
+                doc = reader.read(file);
+                System.out.println("root : " + doc.getRootElement().getName());
+            } catch (DocumentException e) {
+                e.printStackTrace();
+                return null;
+            }
+            return doc;
+        } catch (URISyntaxException ex) {
+            ex.printStackTrace();
+        }
+        return null;
+    }
+
+    public static Document readXMLFileDocument(String xmlpath, boolean create) {
+        return readXML(null, xmlpath, false, create);
+    }
+
+    public static Document readXMLFileDocument(URL url, boolean create) {
+        return readXML(url, null, true, create);
+    }
+
+    private static void writeXML(URL url, String xmlpath, boolean isUrl, Document doc) {
+
+        FileOutputStream out = null;
+        try {
+            if (isUrl) {
+                out = new FileOutputStream(new File(url.toURI()));
+            } else {
+                out = new FileOutputStream(new File(xmlpath));
+            }
+            OutputFormat format = OutputFormat.createPrettyPrint();   //漂亮格式：有空格换行
             format.setEncoding("UTF-8");
-            XMLWriter writer=new XMLWriter(out,format);
+            XMLWriter writer = new XMLWriter(out, format);
             //2.写出Document对象
             writer.write(doc);
             //3.关闭流
             writer.close();
         } catch (IOException ex) {
-            log.log(Level.SEVERE, null, ex);
+            ex.printStackTrace();
+        } catch (URISyntaxException ex) {
+            ex.printStackTrace();
         } finally {
             try {
                 out.close();
             } catch (IOException ex) {
-                log.log(Level.SEVERE, null, ex);
+                ex.printStackTrace();
             }
         }
+    }
+
+    public static void writeXMLFile(URL url, Document doc) {
+        writeXML(url, null, true, doc);
+    }
+
+    public static void writeXMLFile(String xmlpath, Document doc) {
+        writeXML(null, xmlpath, false, doc);
     }
 }
